4356a2fa1cb004b48abc0d0feebce67e827b51d0
Niels Leenheer <niels@leenheer.nl>
2024-11-06 09:56:01 +0100

    Rewrite with better symbology detection and timing for larger 2d barcodes

--- README.md
+++ README.md
@@ -18,7 +18,11 @@ By default most barcode scanners emulate a keyboard meaning all numbers and lett
 
 Becauses of the nature of the library - it reads keyboard input - it can be easily tricked by very quickly typing on your keyboard. If you quickly slide your finger over the numbers on your keyboard it may get picked up as a barcode. There is nothing that can be done about this. The timings have been chosen so that in normal use it won't confuse typing with the output of a barcode scanner. 
 
-One other note, while most barcode scanners type very quickly for a human, it is quite slow for computers. If you scan are very large 2d barcode it may take a second or two before the scanner is finished typing. If you want your application to respond faster, you may be able to enable serial mode or HID mode for your barcode scanner and use one of the sister libraries: [WebHidBarcodeScanner](https://github.com/NielsLeenheer/WebHidBarcodeScanner) or [WebSerialBarcodeScanner](https://github.com/NielsLeenheer/WebSerialBarcodeScanner)
+One other note, while most barcode scanners type very quickly for a human, it is quite slow for computers. If you scan are very large 2D barcode it may quite a bit of time before the scanner is finished typing. Picking the right barcode scanner is also important. For example Honeywell scanners type very quickly - on average 10ms between keystrokes. But Zebra scanners are much slower, on average 80ms, with peaks up to 100ms. That means a QR code with around 250 characters may take up to 25 seconds to type out!
+
+Another problem is that some more complex barcodes may include characters that are not letters, numbers or punctuation. But for example Tabs, and Enters. And if the scanner types this out in the browser window, it may "tab" it's way to the browser location bar, type some data and press Enter. So you end up doing a search on one part of the data in the barcode.
+
+If you want your application to respond faster or be more accurate, you may be able to enable serial mode or HID mode for your barcode scanner and use one of the sister libraries: [WebHidBarcodeScanner](https://github.com/NielsLeenheer/WebHidBarcodeScanner) or [WebSerialBarcodeScanner](https://github.com/NielsLeenheer/WebSerialBarcodeScanner)
 
 <br>
 
@@ -81,13 +85,37 @@ barcodeScanner.addEventListener('disconnected', () => {
 
 ### Symbology 
 
-Because the barcodes are `typed` by the scanner, we can not get any information about the symbology, only the data of the barcode itself. However the library can make an educated guess based on the content. For example, if it starts with `http` it usually is a QR code. If it is 13 digits, it is usually an EAN13 code and 12 is usually a UPC-A. 
+Because the barcodes are `typed` by the scanner, we may not get any information about the symbology, only the data of the barcode itself. 
+
+However the library can make an educated guess based on the content. For example, if it starts with `http` it usually is a QR code. If it is 13 digits and the last digit is a check digit, it is usually an EAN13 code and similarly with 12 digits is usually a UPC-A. 
 
 By default this behaviour is turned off. If you want this library to guess the symbology you can turn it on:
 
 ```js
 const barcodeScanner = new KeyboardBarcodeScanner({
     guessSymbology: true
+});
+```
+
+On some scanners it may actually be possible to add an AIM Code ID as a prefix to the barcode. This is a 3 character identifier for the symbology of the barcode. To enable this for your barcode scanner you may need to scan a configuration barcode. For more information look at the documentation of your barcode scanner. If this AIM Code ID is detected, it will be used to determine the symbology of the barcode.
+
+By default this library will return barcodes of every symbology. However if you want to use this library in a specific environment, such as retail, you can limit this library to only allow symbologies that are used in retail, for example: 
+
+```js
+const barcodeScanner = new KeyboardBarcodeScanner({
+    allowedSymbologies: [ 'ean13', 'ean8', 'upca', 'upce', 'qr-code' ]
+});
+```
+
+This will allow all EAN and UPC barcodes. But also QR-codes because the retail industry is moving to the QR code based GS Digital Links in the coming years. These digital links contain an URL and can be used by consumers to read more about the product they are buying or have bought. But it also includes the Global Trade Identification Number (GTIN) that is also used by EAN and UPC barcodes. 
+
+If we find GS1 data such as the GTIN in the scanned barcode we will automatically decode it and place it in the data property:
+
+```js
+barcodeScanner.addEventListener('barcode', e => {
+    if (e.data?.gtin) {
+        console.log(`Found barcode with GTIN ${e.data.gtin}`);
+    }
 });
 ```
 
@@ -111,16 +139,65 @@ The callback is passed an object with the following properties:
 
 -   `value`<br>
     The value of the barcode as a string
+-   `data`<br>
+    If the barcode contains GS1 data, such as the Global Trade Identification Number (GTIN) the data will be parsed into elements.
+-   `aim`<br>
+    Optionally, the AIM Code ID, which is a 3 character ISO/IEC identifier and gives information about the symbology of the barcode which was scanned. 
 -   `symbology`<br>
     Optionally a library specific identifier of the symbology. 
+-   `guess`<br>
+    If the symbology of this barcode is a guess, then `true`. If we are quite certain of the symbology, then `false`.
+-   `bytes`<br>
+    The raw bytes we've received from the scanner. This propery is an array containing one or more `Uint8Array`'s.
 
+#### Parsed GS1 data
 
+The `data` property is optional, but if GS1 data is detected, it will contain an object with the following properties:
+
+-   `gtin`<br>
+    Optionally, if the GS1 elements define a GTIN, it will be listed here for quick reference.
+-   `elements`<br>
+    An array of all the GS1 elements that the barcode contains. Each element is an object with the folowing properties; `ai`: the appication identifier, `label`: a human readable label and `value`: the value of the element.
+
+#### Symbologies
+
+The `symbology` property can be any of the following common values for 1D barcodes:
+
+`ean8`, `ean13`, `upca`, `upce`, `code39`, `code93`, `code128`, `codabar`, `interleaved-2-of-5`, `gs1-databar-omni`, `gs1-databar-expanded`
+
+Or these 2D barcodes:
+
+`qr-code`, `data-matrix`, `aztec-code`, `pdf417`
+
+#### Example
+
+A typical EAN 13 barcode would look like:
+
+```js
+{
+    value: "3046920029759",
+    symbology: "ean13",
+    guess: false,
+    data: {
+        gtin: "03046920029759",
+        elements: [{
+            ai: "01",
+            label: "GTIN",
+            value: "03046920029759"
+        }]
+    },
+    bytes: [[
+        0x30, 0x33, 0x30, 0x34, 0x36, 0x39, 0x32, 0x30, 
+        0x30, 0x32, 0x1D, 0x37, 0x35, 0x39
+    ]]
+}
+```
 <br>
 
 -----
 
 <br>
 
-This library has been created by Niels Leenheer under the [MIT license](LICENSE). Feel free to use it in your products. The  development of this library is sponsored by Salonhub.
+This library has been created by Niels Leenheer under the [MIT license](LICENSE). Feel free to use it in your products. The development of this library is sponsored by Salonhub.
 
-<a href="https://salohub.nl"><img src="https://salonhub.nl/assets/images/salonhub.svg" width=140></a>
+<a href="https://salonhub.nl"><img src="https://salonhub.nl/assets/images/salonhub.svg" width=140></a>
--- src/main.js
+++ src/main.js
@@ -1,25 +1,60 @@
 import EventEmitter from './event-emitter.js';
-import SymbologyDetector from './symbology-detector.js';
+import { Aim, GS1, Detector } from '@point-of-sale/barcode-parser';
 
 class KeyboardBarcodeScanner {
 
     #options;
     #internal;
 
+    /* 
+        Time between keystrokes for common barcode scanners:
+
+        Netum NSL-5:                6ms
+        Netum NSC750:               19ms
+        Honeywell HF680:            10ms
+        Honeywell Voyager 1400g:    10ms
+        Datalogic BC2090:           1ms
+
+    */
+
+    #timeBetweenKeystrokes = 40;
+
+    /* 
+        Our timeout needs to be set quite conservatively,
+        because the browser may not be able to keep up with
+        the barcode scanner and may pause events for a couple
+        of tens of ms.
+    */
+
+    #timeoutAfterLastKeystroke = 200;
+
+
+
 	constructor(options) {
         this.#options = Object.assign({
+            timing: 'auto',
             guessSymbology: false,
-            debug: false
+            allowedSymbologies: [],
         }, options || {})
 
         this.#internal = {
+            state:          'unknown',
+            command:        [],
             keydown:        this.#keydown.bind(this),
             interval:       null,
             emitter:        new EventEmitter(),
-            buffer:         '',
-            timestamp:      0,
+            buffer:         [],
+            keystrokes:     0,
+            timestamp:      {
+                first:      null,
+                last:       null
+            },
         };
 
+        if (this.#options.timing !== 'auto' && typeof this.#options.timing === 'number') {
+            this.#timeBetweenKeystrokes = this.#options.timing
+            this.#timeoutAfterLastKeystroke = this.#options.timing * 5;
+        }
 	}
 
 	async connect() {
@@ -31,6 +66,7 @@ class KeyboardBarcodeScanner {
 	}
 
 	async disconnect() {
+        this.#close();
 	}
 
 	async #open() {
@@ -50,62 +86,199 @@ class KeyboardBarcodeScanner {
     }
 
     #keydown(e) {
+        let now = performance.now();
 
-        /* Optional: Clear buffer on NumLock */
+        /* Do not process if the event target is a form field */
 
-        if (e.key === 'Clear' && e.code === 'NumLock') {
-            this.#internal.buffer = '';
-            this.#internal.timestamp = e.timeStamp;
+        if (e.target.tagName === 'INPUT' || e.target.tagName === 'TEXTAREA') {
             return;
+        }
+
+        /* Prevent this keydown from reaching the browser when we're in data or command mode */
+
+        if (this.#internal.state !== 'unknown') {
+            e.stopPropagation();
+            e.preventDefault();    
+        }
+
+        console.log(e);
+
+        /* Set the starting timestamp for this - perhaps series of - keydown events */
+
+        if (this.#internal.timestamp.first === null) {
+            this.#internal.timestamp.first = now; // e.timeStamp;
         }
 
         /* Parse buffer on timeout */
 
-        if (e.timeStamp - this.#internal.timestamp > 30) {
-            this.#parse(this.#internal.buffer);
-            this.#internal.buffer = '';
+        if (this.#internal.keystrokes > 1 && this.#internal.state === 'unknown') {
+            if (now - this.#internal.timestamp.last > this.#timeBetweenKeystrokes) {
+                console.log(`forcing parse because ${this.#timeBetweenKeystrokes}ms since last keydown`, now, this.#internal.timestamp.last, now - this.#internal.timestamp.last);
+                this.#parse(this.#internal.buffer);
+                this.#reset();
+            }
+        }
+
+        /* If we've received more than 2 keystrokes in less than 30ms each, we're in data mode */
+
+        if (this.#internal.keystrokes > 2 && this.#internal.state === 'unknown') {
+            if (now - this.#internal.timestamp.first < this.#internal.keystrokes * 30) {
+                this.#internal.state = 'data';
+            }
+        }
+
+        /* Intercept commands */
+
+        if (e.key === 'Clear' && e.code === 'NumLock') {
+
+            if (this.#internal.state !== 'command') {
+                this.#internal.state = 'command';
+            }
+
+            else if (this.#internal.state === 'command') {
+                this.#command();
+                this.#internal.state = 'data';
+            }
+
+            if (this.#internal.buffer.length > 0) {
+                this.#internal.keystrokes++;
+                this.#internal.timestamp.last = now;
+            }
+
+            return;
         }
 
         /* Append key to buffer */
 
-        if (e.key.length === 1) {
-            this.#internal.buffer += e.key;
+        if (this.#internal.state === 'unknown' || this.#internal.state === 'data') {
+
+            /* Just a regular keypress */
+
+            if (e.key.length === 1) {
+                this.#internal.buffer.push(e.key.charCodeAt(0));
+            }
+
+            /* Special keys */
+            
+            else {
+                let keyMapping = {
+                    'Enter':    0x0d,
+                    'Tab':      0x09,
+                    'Escape:':  0x1b
+                }
+
+                if (keyMapping[e.key]) {
+                    this.#internal.buffer.push(keyMapping[e.key]);
+                }
+            }
+        }
+        else {
+            this.#internal.command.push({ key: e.key, code: e.code });
         }
 
-        this.#internal.timestamp = e.timeStamp;
+        this.#internal.keystrokes++;
+        this.#internal.timestamp.last = now;
     }
 
     #check() {
+        let now = performance.now();
+
         if (this.#internal.buffer.length === 0) {
             return;
         }
 
-        if (performance.now() - this.#internal.timestamp > 50) {
+        if (now - this.#internal.timestamp.last > this.#timeoutAfterLastKeystroke) {
+            console.log(`forcing parse because ${this.#timeoutAfterLastKeystroke}ms have passed`, now, this.#internal.timestamp.last, now - this.#internal.timestamp.last);
             this.#parse(this.#internal.buffer);
-            this.#internal.buffer = '';
+            this.#reset();
         }
     }
 
+    #command() {
+        if (this.#internal.command.length === 0) {
+            return;
+        }
+
+        let command = this.#internal.command.shift();
+
+        if (command.code == 'AltLeft') {
+            let payload = this.#internal.command.map(c => c.key).join('');
+            this.#internal.buffer.push(parseInt(payload, 10));
+        }
+
+        this.#internal.command = [];
+    }
+
     #parse(buffer) {
-        if (buffer.length > 6) {
+        if (buffer.length > 4) {
             
-            let data = {
-                value: buffer
+            console.log(
+                `received ${this.#internal.keystrokes} keystrokes in ${parseInt(this.#internal.timestamp.last - this.#internal.timestamp.first, 10)}ms, ` + 
+                `that is an average of ${parseInt((this.#internal.timestamp.last - this.#internal.timestamp.first) / this.#internal.keystrokes, 10)}ms per keystroke`
+            );
+
+            let result = {
+                value: String.fromCharCode.apply(null, buffer),
+                bytes: [
+                    new Uint8Array(buffer)
+                ]
             };
 
-            /* Try to guess the symbology */
+            /* If the last character of value is a carriage return, remove it */
 
-            if (this.#options.guessSymbology) {
-                let symbology = SymbologyDetector.detect(buffer);
+            if (result.value.endsWith('\r')) {
+                result.value = result.value.slice(0, -1);
+            }
 
-                if (symbology) {
-                    data.symbology = symbology;
-                    data.guess = true;
+            /* Check if we have and AIM identifier */
+
+            if (result.value.startsWith(']')) {
+                let aim = Aim.decode(result.value.substr(0, 3), result.value.substr(3));
+
+                if (aim) {
+                    result.aim = result.value.substr(0, 3);
+                    result.symbology = aim;
+                }
+
+                result.value = result.value.substr(3);
+            }
+
+            /* Otherwise try to guess the symbology */
+
+            else if (this.#options.guessSymbology) {
+                let detected = Detector.detect(result.value);
+
+                if (detected) {
+                    result = Object.assign(result, detected);
                 }
             }
 
-            this.#internal.emitter.emit('barcode', data);
+            /* Decode GS1 data */
+
+            let parsed = GS1.parse(result);
+            if (parsed) {
+                result.data = parsed;
+            }
+
+            /* Emit the barcode event */
+
+            if (this.#options.allowedSymbologies.length === 0 ||
+                this.#options.allowedSymbologies.includes(result.symbology)) 
+            {
+                this.#internal.emitter.emit('barcode', result);
+            }
         }
+    }
+
+    #reset() {
+        this.#internal.buffer = [];
+        this.#internal.command = [];
+        this.#internal.state = 'unknown';
+        this.#internal.keystrokes = 0;
+        this.#internal.timestamp = {
+            first:      null,
+            last:       null
+        };
     }
 
 	addEventListener(n, f) {
